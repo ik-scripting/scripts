e354fa03f62e049075741d08f826c4a5ec3a7b9f
Ivan Katliarchuk <5395690+ivankatliarchuk@users.noreply.github.com>
2020-01-18 20:07:04 +0000

    Create 002-latest-tag.sh

---
+++ git/002-latest-tag.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# The command finds the most recent tag that is reachable from a commit.
+# If the tag points to the commit, then only the tag is shown.
+# Otherwise, it suffixes the tag name with the number of additional commits on top of the tagged object 
+# and the abbreviated object name of the most recent commit.
+git describe
+
+# With --abbrev set to 0, the command can be used to find the closest tagname without any suffix:
+git describe --abbrev=0
+
+# other examples
+git describe --abbrev=0 --tags # gets tag from current branch
+git describe --tags `git rev-list --tags --max-count=1` # g
