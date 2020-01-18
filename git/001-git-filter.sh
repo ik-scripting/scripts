8ae27a56ec8ac303d23bdaf634ff532dc6c45bd3
Ivan Katliarchuk <5395690+ivankatliarchuk@users.noreply.github.com>
2020-01-18 18:19:03 +0000

    commit filter

---
+++ git/001-git-filter.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Get last commit message
+readonly local last_commit_log=$(git log -1 --pretty=format:"%s")
+echo "last commit log: $last_commit_log"
+
+readonly local filter_count=$(echo "$last_commit_log" | grep -c "$COMMIT_FILTER" )
+echo "number of occurence of '$COMMIT_FILTER' in '$last_commit_log': $filter_count"
+
+if [[ "$filter_count" -eq 0 ]]; then
+  echo "all good, continue"
+else
+  echo "the last commit log \"$last_commit_log\" contains \"$COMMIT_FILTER\", stopping"
+  exit 78
+fi
