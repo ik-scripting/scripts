0b97d0c2f7c826072a5a75cf169a70ad28b37bc3
Ivan Katliarchuk <5395690+ivankatliarchuk@users.noreply.github.com>
2020-05-28 20:15:04 +0100

    Create 001.args.sh

---
+++ optargs/001.args.sh
@@ -0,0 +1,12 @@
+for i in "$@"; do
+    case $1 in
+        -s|--script) SCRIPT="$2"; shift ;;
+        -t|--target) TARGET="$2"; shift ;;
+        -c|--configuration) CONFIGURATION="$2"; shift ;;
+        -b|--buildconfiguration) BUILDCONFIGURATION="$2"; shift ;; # Added parameter
+        -v|--verbosity) VERBOSITY="$2"; shift ;;
+        -d|--dryrun) DRYRUN=true ;;
+        --version) SHOW_VERSION=true ;;
+    esac
+    shift
+done
