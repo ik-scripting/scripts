48144707c69a2d375a0602b01abd2891b92bfc5f
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2023-09-05 14:43:25 +0100

    update rds scripts
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- aws/rds.sh
+++ aws/rds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # https://www.dba-ninja.com/2022/09/aws-cli-cheatsheet.html
 
-# List aurora dbs with maintance windows
-aws rds describe-db-clusters --filters Name=engine,Values=aurora-postgresql --query "DBClusters[*].{Name:DBClusterIdentifier,Version:EngineVersion,Maintance:PreferredMaintenanceWindow}" --output=table
+# List aurora dbs with maintance windows, and teams
+aws rds describe-db-clusters --filters Name=engine,Values=aurora-postgresql --query "DBClusters[*].{Team:TagList[?Key=='team'].Value | [0],Name:DBClusterIdentifier,Version:EngineVersion,Maintance:PreferredMaintenanceWindow}" --output=table
