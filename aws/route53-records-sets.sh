cda2073b9a3c795c5bc1c9db251ae80ff2b9de61
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2024-01-16 20:20:07 +0000

    added aws cli route53 command
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- aws/route53-records-sets.sh
+++ aws/route53-records-sets.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-HOSTED_ZONE_ID=$1
+HOSTED_ZONE_ID=Z1HLSICRCJVNZR
 
 aws route53 list-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID
+
+# how to use contains in query
+aws route53 list-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --query "ResourceRecordSets[?contains(Name, 'plm-category') == \`true\`]"
 
 aws route53 list-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --query "ResourceRecordSets[?Name == 'datadog-agent-internal-eks.eu-west-1.dev.hbi.systems.'].ResourceRecords"
 
@@ -11,3 +14,8 @@ aws route53 list-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --query "
 aws route53 list-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --query "ResourceRecordSets[?Name == 'datadog-agent-internal-eks.eu-west-1.dev.hbi.systems.'].ResourceRecords" --output text > output
 
 aws route53 list-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --query "ResourceRecordSets[?Name == 'datadog-agent-internal-eks.eu-west-1.dev.hbi.systems.'].ResourceRecords[*].Value" --output text > output
+
+
+aws route53 list-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --query "ResourceRecordSets[?Name == 'plm-category-service-\\045\\041s\\050\\074nil\\076\\051-internal-eks.eu-west-1.dev.hbi.systems.']"
+
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch file:///delete.json
