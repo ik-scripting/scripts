f5a5e96306d8fed69931c1f782bc8a4d0271787e
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2024-01-18 19:17:04 +0000

    added extra scripts
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

---
+++ docker/001-e2e_test_linux_darwin.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# https://github.com/opentofu/opentofu/blob/main/.github/scripts/e2e_test_linux_darwin.sh
+set -uo pipefail
+
+if [[ $arch == 'arm' || $arch == 'arm64' ]]
+then
+    export DIR=$(mktemp -d)
+    unzip -d $DIR "${e2e_cache_path}/tofu-e2etest_${os}_${arch}.zip"
+    unzip -d $DIR "./tofu_${version}_${os}_${arch}.zip"
+    sudo chmod +x $DIR/e2etest
+    docker run --platform=linux/arm64 -v $DIR:/src -w /src arm64v8/alpine ./e2etest -test.v
+else
+    unzip "${e2e_cache_path}/tofu-e2etest_${os}_${arch}.zip"
+    unzip "./tofu_${version}_${os}_${arch}.zip"
+    TF_ACC=1 ./e2etest -test.v
+fi
