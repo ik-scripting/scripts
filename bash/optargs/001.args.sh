6a9b8d1327f9db978e4e1bf9de5002e999a92546
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2024-01-26 16:21:26 +0000

    chore: update kubectl
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- bash/optargs/001.args.sh
+++ bash/optargs/001.args.sh
@@ -10,3 +10,28 @@ for _ in "$@"; do
     esac
     shift
 done
+
+# updated version
+
+function k.get-list {
+    usage() { echo "k.get-list: [--kind <resource kind> --api <api> --version <api version>]" 1>&2; }
+
+    if [ "$#" -lt 6 ]; then
+        echo "Not enough arguments... Should be '6' provided '$#'"
+        usage
+    else
+
+        local kind api version
+        for _ in "$@"; do
+            case $1 in
+                -k|--kind) kind="$2"; ;;
+                -t|--api) api="$2"; ;;
+                -c|--version) version="$2"; ;;
+                --help) usage ;;
+            esac
+            shift
+        done
+        set -x
+        kubectl get ${kind}.${version}.${api}
+    fi
+}
